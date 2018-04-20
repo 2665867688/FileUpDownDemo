8781a1a30c949ba548fdf024cb2655e76acc7fd0
shimy <2665867688@qq.com>
2018-04-20 15:26:17 +0800

    简单下载

---
+++ app/src/main/java/com/yue/fileupdown/constant/Constanct.java
@@ -0,0 +1,11 @@
+package com.yue.fileupdown.constant;
+
+/**
+ * time：2018/4/20 15:25
+ * author：shimy
+ * classname：Constanct
+ * description：存储常量
+ */
+
+public class Constanct {
+}
---
+++ app/src/main/java/com/yue/fileupdown/ui/download/TDownLoadActivity.java
@@ -0,0 +1,22 @@
+package com.yue.fileupdown.ui.download;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yue.fileupdown.R;
+
+/**
+ * time：2018/4/20 15:24
+ * author：shimy
+ * classname：TDownLoadActivity
+ * description：简单测试下下载
+ */
+
+public class TDownLoadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tdown_load);
+    }
+}
