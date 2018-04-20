8d50744017d75ba04d684083083e444b7bd1cccd
shimy <2665867688@qq.com>
2018-04-20 16:01:38 +0800

    简单下载

--- app/src/main/java/com/yue/fileupdown/ui/download/TDownLoadActivity.java
+++ app/src/main/java/com/yue/fileupdown/ui/download/TDownLoadActivity.java
@@ -1,14 +1,21 @@
 package com.yue.fileupdown.ui.download;
 
+import android.Manifest;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.tbruyelle.rxpermissions2.Permission;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.yue.fileupdown.R;
 import com.yue.fileupdown.constant.Constanct;
 import com.yue.fileupdown.databinding.ActivityTdownLoadBinding;
+
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
 
 /**
  * time：2018/4/20 15:24
@@ -25,6 +32,7 @@ public class TDownLoadActivity extends AppCompatActivity implements View.OnClick
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_tdown_load);
+        requestPermissions();
         mBinding.btnTdlStart.setOnClickListener(this);
         mBinding.btnTdlCancle.setOnClickListener(this);
     }
@@ -54,5 +62,42 @@ public class TDownLoadActivity extends AppCompatActivity implements View.OnClick
     private void cancleDl() {
 
     }
+
+    private void requestPermissions() {
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions
+                .requestEachCombined(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Consumer<Permission>() {
+                               @Override
+                               public void accept(Permission permission) throws Exception {
+                                   if (permission.granted) {
+                                       // 所有权限都已授权
+//                                       Toast.makeText(TSimpleMapActivity.this, "权限请求成功", Toast.LENGTH_SHORT).show();
+                                   } else if (permission.shouldShowRequestPermissionRationale) {
+                                       // 至少有一个权限未被授予 没有选中 [不在询问按钮]
+                                       Toast.makeText(TDownLoadActivity.this, "有权限未申请成功" + permission.name, Toast.LENGTH_SHORT).show();
+                                       if (permission.name.equals(Manifest.permission.ACCESS_COARSE_LOCATION) || permission.name.equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                                           Toast.makeText(TDownLoadActivity.this, "定位权限未被授予", Toast.LENGTH_SHORT).show();
+                                       } else if (permission.name.equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                                           Toast.makeText(TDownLoadActivity.this, "存储权限未被授予", Toast.LENGTH_SHORT).show();
+                                       }
+                                   } else {
+                                       // 至少有一个权限未被授予 选中了[不在询问按钮]
+                                   }
+                               }
+                           }, new Consumer<Throwable>() {
+                               @Override
+                               public void accept(Throwable t) {
+                                   Toast.makeText(TDownLoadActivity.this, "权限请求错误", Toast.LENGTH_SHORT).show();
+                               }
+                           },
+                        new Action() {
+                            @Override
+                            public void run() {
+//                                Toast.makeText(TSimpleMapActivity.this, "权限请求完成", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+    }
+
 
 }
