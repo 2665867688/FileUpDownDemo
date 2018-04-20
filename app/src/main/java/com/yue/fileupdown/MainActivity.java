15bb9cb9840873eaf1499d242ccc09e456cfdbe5
shimy <2665867688@qq.com>
2018-04-20 11:01:32 +0800

    文件操作

---
+++ app/src/main/java/com/yue/fileupdown/MainActivity.java
@@ -0,0 +1,21 @@
+package com.yue.fileupdown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * time：2018/4/20 10:59
+ * author：shimy
+ * classname：MainActivity
+ * description：文件上传下载
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
