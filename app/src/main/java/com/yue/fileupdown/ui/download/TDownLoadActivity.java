b24d67e6c4560bed14136bcfaedd339d869d36f4
shimy <2665867688@qq.com>
2018-04-20 15:41:17 +0800

    简单下载

--- app/src/main/java/com/yue/fileupdown/ui/download/TDownLoadActivity.java
+++ app/src/main/java/com/yue/fileupdown/ui/download/TDownLoadActivity.java
@@ -1,9 +1,11 @@
 package com.yue.fileupdown.ui.download;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.yue.fileupdown.R;
+import com.yue.fileupdown.databinding.ActivityTdownLoadBinding;
 
 /**
  * time：2018/4/20 15:24
@@ -14,11 +16,15 @@ import com.yue.fileupdown.R;
 
 public class TDownLoadActivity extends AppCompatActivity {
 
+    private ActivityTdownLoadBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tdown_load);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_tdown_load);
     }
+
+
 
 
 }
